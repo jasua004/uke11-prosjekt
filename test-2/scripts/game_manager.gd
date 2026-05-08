9007a177cdb80659740c4fdb30bcb232be8a0cd1
jasua004 <jasua004@osloskolen.no>
2026-05-08 10:48:45 +0200

    speedrun timer

--- test-2/scripts/game_manager.gd
+++ test-2/scripts/game_manager.gd
@@ -1,8 +1,24 @@
 extends Node
 
 var score = 0
+var elapsed_time: float = 0.0
+var timer_running: bool = false
+
+@onready var door = $"../Door"
+
+func _ready():
+	timer_running = true
+
+func _process(delta):
+	if timer_running:
+		elapsed_time += delta
 
 func add_point():
 	score += 1
 	print(score)
-	
+	if score == 6:
+		door.unlock()
+
+func level_complete():
+	timer_running = false
+	print("Time: ", elapsed_time)
